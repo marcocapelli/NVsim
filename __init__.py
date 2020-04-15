788b8601355518804db27cd4d30ebde027061dc9
marcocapelli <marco.capelli@rmit.edu.au>
2020-04-15 18:34:09 +1000

    Fixed __init__ to make it work as an external library

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
-from libs.setup import Setup
-from libs.ensembleNV import EnsembleNV
-from libs.singleNV import SingleNV
+from .libs.setup import Setup
+from .libs.ensembleNV import EnsembleNV
+from .libs.singleNV import SingleNV
